2ab85b549e78135059f856e6e8390a4a3c52e26d
HardyHu <1243971719@qq.com>
2025-10-08 15:12:38 +0800

    feat: enhance auto_login function to support dynamic browser selection

--- login_bot.py
+++ login_bot.py
@@ -3,10 +3,23 @@ from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 
 
-def auto_login(url, username, password):
+def auto_login(url, username, password, browser_type=None, headless=True):
+    # 如果未指定浏览器，则根据系统自动选择：Linux 优先使用 firefox，其他默认 chromium
+    if browser_type is None:
+        if os.name == "posix" and "linux" in os.uname().sysname.lower():
+            browser_type = "firefox"
+        else:
+            browser_type = "chromium"
+
     with sync_playwright() as p:
-        # 使用内置的 Chromium
-        browser = p.chromium.launch(headless=True)  # False 表示显示浏览器
+        # 动态选择浏览器
+        if browser_type == "firefox":
+            browser = p.firefox.launch(headless=headless)
+        elif browser_type == "webkit":
+            browser = p.webkit.launch(headless=headless)
+        else:
+            browser = p.chromium.launch(headless=headless)  # 默认使用 Chromium
+
         page = browser.new_page()
 
         try:
